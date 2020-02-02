9a96c0b39a442aefb2cdcd805c1a13ce3161e2f4
hujinbo <hujinbo1995@126.com>
2020-02-02 13:42:34 +0800

    Spring Initializr

---
+++ src/main/java/me/hujinbo/boot/example/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package me.hujinbo.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExampleApplication.class, args);
+    }
+
+}
